dcf6aadf0c4dc130041fce12f3eefa45de550ccb
Alexander Smith <alexander.smith@just-eat.com>
2017-07-25 16:31:47 +0100

    Initial commit

---
+++ KeepAlive/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net.NetworkInformation;
+using System.Text;
+
+namespace KeepAlive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args == null)
+            {
+                Console.WriteLine("You forgot to pass in the IP to ping!");
+                return;
+            }
+
+            var url = args[0];
+
+            new Start().Run(url);
+        }
+    }
+
+    public class Start
+    {
+		public async void Run(string url)
+		{
+
+			//bool pingable = false;
+			Ping pinger = new Ping();
+			byte[] buffer = Encoding.ASCII.GetBytes(ip);
+
+            while (true)
+            {
+                try
+                {
+                    PingReply reply = await pinger.SendPingAsync(url);
+
+                    if (reply.Status == IPStatus.Success)
+                    {
+                        Console.WriteLine("");
+                    }
+
+                    //pingable = reply.Status == IPStatus.Success;
+                }
+                catch (PingException)
+                {
+                    // Discard PingExceptions and return false;
+                }
+            }
+		}
+    }
+}
